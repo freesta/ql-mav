3b1d788dce7c818012b3affbdbbc0849b980fded
freesta <freesta1@gmx.de>
2014-11-01 21:00:40 +0100

    UI changes, moved qlmav.ico
    Smaller font and tooltips added.

--- source/qlmav.cpp
+++ source/qlmav.cpp
@@ -28,21 +28,22 @@ TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
 
 HWND text_box = NULL;
 HWND graph_box = NULL;
-HWND sens_box= NULL;
-HWND accel_box= NULL;
-HWND offset_box= NULL;
-HWND senscap_box= NULL;
-HWND power_box= NULL;
+HWND sens_box = NULL;
+HWND accel_box = NULL;
+HWND offset_box = NULL;
+HWND senscap_box = NULL;
+HWND power_box = NULL;
 HWND m_cpi_box = NULL;
 HWND fps_box = NULL;
 HWND x_scale_box = NULL;
 HWND y_scale_box = NULL;
 HWND controlcontainer = NULL;
+HWND caption_box = NULL;
 
-double x_max = 8;
-double y_max = 8;
+double x_max=8;
+double y_max=8;
 long graph_x_max=400;
-long graph_y_max=200;
+long graph_y_max=209;
 double speed=0.0;
 double multi=0.0;
 double accel=3;
@@ -52,25 +53,30 @@ double offset=0.4;
 double power=2;
 double m_cpi=400;
 double fps=125;
+double cur_fps=0.0;
 long lastx=0;
 long lasty=0;
 
-RECT invalidate_rect={10,10,410,210};
+RECT invalidate_rect = {10,10,410,219};
+RECT invalidate_rect_2 = {0,0,400,100};
+
 BITMAP bitmap;
 RECT last_pixel;
 HBRUSH red=CreateSolidBrush(0x000000FF);
 HBRUSH white=CreateSolidBrush(0x00FFFFFF);
 HBRUSH black=CreateSolidBrush(0x00000000);
-HDC          hdc_backbuffer;
-HBITMAP      hbm_backbuffer;
-HANDLE       h_old;
+HDC          hdc_backbuffer, hdc_backbuffer_2;
+HBITMAP      hbm_backbuffer, hbm_backbuffer_2;
+HANDLE       h_old, h_old_2;
+HFONT standard_font;
 
 auto t_start = std::chrono::high_resolution_clock::now();
 
 TCHAR global_temp_string[300];
-TCHAR speed_unit_str_cpms[10]=L"count/ms";
+TCHAR info_string[300];
+TCHAR speed_unit_str_cpms[10]=L"counts/ms";
 TCHAR speed_unit_str_cmps[10]=L"cm/s";
-TCHAR move_unit_str_cpms[21]=L"deg/(count*0.022)";
+TCHAR move_unit_str_cpms[21]=L"deg/(counts*0.022)";
 TCHAR move_unit_str_cmps[10]=L"deg/cm";
 TCHAR *speed_unit_str=speed_unit_str_cpms;
 TCHAR *move_unit_str=move_unit_str_cpms;
@@ -92,6 +98,9 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
   // TODO: Place code here.
   MSG msg;
   HACCEL hAccelTable;
+  standard_font = CreateFont (13, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, 
+    OUT_TT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, 
+    DEFAULT_PITCH | FF_DONTCARE, TEXT("MS Shell Dlg"));
 
   // Initialize global strings
   LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
@@ -151,8 +160,8 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
   //  hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
   //     CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
-  hWnd = CreateWindow(szWindowClass, szTitle,  WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
-    CW_USEDEFAULT, CW_USEDEFAULT, 545, 360, NULL, NULL, hInstance, NULL);
+  hWnd = CreateWindow(szWindowClass, szTitle,  WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_CLIPCHILDREN,
+    CW_USEDEFAULT, CW_USEDEFAULT, 525, 330, NULL, NULL, hInstance, NULL);
 
   if (!hWnd)
   {
@@ -169,17 +178,19 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
   rid[0].usUsage = 0x02; 
   rid[0].dwFlags = RIDEV_INPUTSINK;  
   rid[0].hwndTarget = hWnd;
-
-
+      
 
   if (RegisterRawInputDevices(rid, 1, sizeof(rid[0])) == FALSE) {
     //registration failed. Call GetLastError for the cause of the error
+    MessageBox(NULL,L"Could not register mouse input.",L"Error",MB_OK);
   }
+
+
   return TRUE;
 }
 
 // Tooltips are not used at the moment.
-void CreateToolTipForRect(HWND hwndParent, HINSTANCE ginst, TCHAR* TEXT)
+void CreateToolTipForRect(HWND hwndParent, HINSTANCE ginst, TCHAR* text)
 {
   // Create a tooltip.
   HWND hwndTT = CreateWindowEx(WS_EX_TOPMOST, TOOLTIPS_CLASS, NULL, 
@@ -190,23 +201,23 @@ void CreateToolTipForRect(HWND hwndParent, HINSTANCE ginst, TCHAR* TEXT)
   SetWindowPos(hwndTT, HWND_TOPMOST, 0, 0, 0, 0, 
     SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
 
+
   // Set up "tool" information. In this case, the "tool" is the entire parent window.
 
   TOOLINFO ti = { 0 };
-  ti.cbSize   = sizeof(TOOLINFO);
+  ti.cbSize   = sizeof(TOOLINFO) - 4; // The -4 is due to a common windows bug. TODO: Enable Visual Styles and remove -4.
   ti.uFlags   = TTF_SUBCLASS;
   ti.hwnd     = hwndParent;
   ti.hinst    = ginst;
-  ti.lpszText = TEXT("This is your tooltip string.");
+  ti.lpszText = text;
 
   GetClientRect (hwndParent, &ti.rect);
 
   // Associate the tooltip with the "tool" window.
-  SendMessage(hwndTT, TTM_ADDTOOL, 0, (LPARAM) (LPTOOLINFO) &ti);	
+  SendMessage(hwndTT, TTM_ADDTOOL, 0, (LPARAM)  (LPTOOLINFO)&ti);	
 } 
 
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK TextBoxProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
   int wmId, wmEvent;
   PAINTSTRUCT ps;
@@ -218,120 +229,214 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
   switch (message)
   {
-  case WM_GETMINMAXINFO:
-    {
-      LPMINMAXINFO p_info = (LPMINMAXINFO)lParam;
-      int total_border_width = 2 * GetSystemMetrics( SM_CXFRAME );
-      int total_border_height = 2 * GetSystemMetrics( SM_CYFRAME ) + 
-        GetSystemMetrics( SM_CYCAPTION ) - GetSystemMetrics( SM_CYBORDER );
-      POINT min,max;
+  case WM_PAINT:
+    hdc = BeginPaint(hWnd, &ps);
 
-      min.x = 545 > 0 ? 545+ total_border_width : p_info->ptMinTrackSize.x;
-      min.y = 360 > 0 ? 360 + total_border_height : p_info->ptMinTrackSize.y;
-      max.x = 545 > 0 ? 545  + total_border_width : p_info->ptMaxTrackSize.x;
-      max.y = 360 > 0 ? 360  + total_border_height : p_info->ptMaxTrackSize.y;
+    hdc_backbuffer_2 = CreateCompatibleDC(hdc);
+    hbm_backbuffer_2 = CreateCompatibleBitmap(hdc, 400, 100);
+    h_old_2   = SelectObject(hdc_backbuffer_2, hbm_backbuffer_2);
 
-      p_info->ptMinTrackSize = min;
-      p_info->ptMaxTrackSize = max;
-    }
+    FillRect(hdc_backbuffer_2,&invalidate_rect_2,(HBRUSH)COLOR_WINDOW);
+
+    SetTextColor(hdc_backbuffer_2,0x00000000);
+    SetBkMode(hdc_backbuffer_2,TRANSPARENT);
+    SelectObject(hdc_backbuffer_2,standard_font);
+    DrawText(hdc_backbuffer_2,info_string,wcslen(info_string),&invalidate_rect_2,DT_LEFT | DT_EXTERNALLEADING | DT_WORDBREAK);
+    BitBlt(hdc, 0, 0, 400, 100, hdc_backbuffer_2, 0, 0, SRCCOPY);
+
+
+    SelectObject(hdc_backbuffer_2, h_old_2);
+    DeleteObject(hbm_backbuffer_2);
+    DeleteDC    (hdc_backbuffer_2);
+
+
+    EndPaint(hWnd,&ps);
 
     break;
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
+  default:
+    return DefWindowProc(hWnd, message, wParam, lParam);
+  }
+  return 0;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+  int wmId, wmEvent;
+  PAINTSTRUCT ps;
+  HDC hdc;
+  HRESULT hResult;
+
+  NONCLIENTMETRICS ncm;
+  ncm.cbSize = sizeof(NONCLIENTMETRICS);
+  ::SystemParametersInfo(SPI_GETNONCLIENTMETRICS, sizeof(NONCLIENTMETRICS), &ncm, 0);
+  //HFONT hFont = ::CreateFontIndirect(&ncm.lfMessageFont);
+
+
+
+
+
+  switch (message)
+  {
+
   case WM_CREATE:
+
+
     sens_box = CreateWindow(L"EDIT",
       L"3",
       WS_BORDER | WS_CHILD | WS_VISIBLE,
-      420,10,45,20,
+      420,10,35,17,
       hWnd, NULL,NULL, NULL);
+    SendMessage(sens_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+
     accel_box = CreateWindow(L"EDIT",
       L"0.1",
       WS_BORDER | WS_CHILD | WS_VISIBLE,
-      420,40,45,20,
+      420,34,35,17,
       hWnd, NULL,NULL, NULL);
+    SendMessage(accel_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+
     offset_box = CreateWindow(L"EDIT",
       L"0",
       WS_BORDER | WS_CHILD | WS_VISIBLE,
-      420,70,45,20,
+      420,58,35,17,
       hWnd, NULL,NULL, NULL);
+    SendMessage(offset_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+
     senscap_box = CreateWindow(L"EDIT",
       L"0",
       WS_BORDER | WS_CHILD | WS_VISIBLE,
-      420,100,45,20,
+      420,82,35,17,
       hWnd, NULL,NULL, NULL);
+    SendMessage(senscap_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+
     power_box = CreateWindow(L"EDIT",
       L"2",
       WS_BORDER | WS_CHILD | WS_VISIBLE,
-      420,130,45,20,
+      420,106,35,17,
       hWnd, NULL,NULL, NULL);
+    SendMessage(power_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+
     m_cpi_box = CreateWindow(L"EDIT",
       L"0",
       WS_BORDER | WS_CHILD | WS_VISIBLE,
-      420,160,45,20,
+      420,130,35,17,
       hWnd, NULL,NULL, NULL);
+    SendMessage(m_cpi_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+
     fps_box = CreateWindow(L"EDIT",
       L"125",
       WS_BORDER | WS_CHILD | WS_VISIBLE,
-      420,190,45,20,
+      420,154,35,17,
       hWnd, NULL,NULL, NULL);
+    SendMessage(fps_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+
     x_scale_box = CreateWindow(L"EDIT",
       L"8",
       WS_BORDER | WS_CHILD | WS_VISIBLE,
-      420,220,45,20,
+      420,178,35,17,
       hWnd, NULL,NULL, NULL);
+    SendMessage(x_scale_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+
     y_scale_box = CreateWindow(L"EDIT",
       L"8",
       WS_BORDER | WS_CHILD | WS_VISIBLE,
-      420,250,45,20,
+      420,202,35,17,
       hWnd, NULL,NULL, NULL);
-    CreateWindow(L"STATIC",
+    SendMessage(y_scale_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+
+    caption_box = CreateWindow(L"STATIC",
       L"sens",
-      WS_CHILD | WS_VISIBLE,
-      475,10,45,20,
+      WS_CHILD | WS_VISIBLE | SS_NOTIFY,
+      465,10,45,17,
       hWnd, NULL,NULL, NULL);
-    CreateWindow(L"STATIC",
+    SendMessage(caption_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+    CreateToolTipForRect(caption_box,hInst,L"cl_sensitivity");
+    CreateToolTipForRect(sens_box,hInst,L"cl_sensitivity");
+
+    caption_box = CreateWindow(L"STATIC",
       L"accel",
-      WS_CHILD | WS_VISIBLE,
-      475,40,45,20,
+      WS_CHILD | WS_VISIBLE | SS_NOTIFY,
+      465,34,45,17,
       hWnd, NULL,NULL, NULL);
-    CreateWindow(L"STATIC",
+    SendMessage(caption_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+    CreateToolTipForRect(caption_box,hInst,L"cl_mouseAccel");
+    CreateToolTipForRect(accel_box,hInst,L"cl_mouseAccel");
+
+    caption_box = CreateWindow(L"STATIC",
       L"offset",
-      WS_CHILD | WS_VISIBLE,
-      475,70,45,20,
+      WS_CHILD | WS_VISIBLE | SS_NOTIFY,
+      465,58,45,17,
       hWnd, NULL,NULL, NULL);
-    CreateWindow(L"STATIC",
+    SendMessage(caption_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+    CreateToolTipForRect(caption_box,hInst,L"cl_mouseAccelOffset");
+    CreateToolTipForRect(offset_box,hInst,L"cl_mouseAccelOffset");
+
+    caption_box = CreateWindow(L"STATIC",
       L"cap",
-      WS_CHILD | WS_VISIBLE,
-      475,100,45,20,
+      WS_CHILD | WS_VISIBLE | SS_NOTIFY,
+      465,82,45,17,
       hWnd, NULL,NULL, NULL);
-    CreateWindow(L"STATIC",
+    SendMessage(caption_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+    CreateToolTipForRect(caption_box,hInst,L"cl_mouseSensCap");
+    CreateToolTipForRect(senscap_box,hInst,L"cl_mouseSensCap");
+
+    caption_box = CreateWindow(L"STATIC",
       L"power",
-      WS_CHILD | WS_VISIBLE,
-      475,130,45,20,
+      WS_CHILD | WS_VISIBLE | SS_NOTIFY,
+      465,106,45,17,
       hWnd, NULL,NULL, NULL);
-    CreateWindow(L"STATIC",
+    SendMessage(caption_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+    CreateToolTipForRect(caption_box,hInst,L"cl_mouseAccelPower");
+    CreateToolTipForRect(power_box,hInst,L"cl_mouseAccelPower");
+
+    caption_box = CreateWindow(L"STATIC",
       L"m_cpi",
-      WS_CHILD | WS_VISIBLE,
-      475,160,45,20,
+      WS_CHILD | WS_VISIBLE | SS_NOTIFY,
+      465,130,45,17,
       hWnd, NULL,NULL, NULL);
-    CreateWindow(L"STATIC",
+    SendMessage(caption_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+    CreateToolTipForRect(caption_box,hInst,L"m_cpi");
+    CreateToolTipForRect(m_cpi_box,hInst,L"m_cpi");
+
+    caption_box = CreateWindow(L"STATIC",
       L"fps",
-      WS_CHILD | WS_VISIBLE,
-      475,190,45,20,
+      WS_CHILD | WS_VISIBLE | SS_NOTIFY,
+      465,154,45,17,
       hWnd, NULL,NULL, NULL);
-    CreateWindow(L"STATIC",
-      L"xScale",
-      WS_CHILD | WS_VISIBLE,
-      475,220,45,20,
+    SendMessage(caption_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+    CreateToolTipForRect(caption_box,hInst,L"com_maxfps");
+    CreateToolTipForRect(fps_box,hInst,L"com_maxfps");
+
+    caption_box = CreateWindow(L"STATIC",
+      L"x-scale",
+      WS_CHILD | WS_VISIBLE | SS_NOTIFY,
+      465,178,47,17,
       hWnd, NULL,NULL, NULL);
-    CreateWindow(L"STATIC",
-      L"yScale",
-      WS_CHILD | WS_VISIBLE,
-      475,250,45,20,
+    SendMessage(caption_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+    CreateToolTipForRect(caption_box,hInst,L"scale for x-axis");
+    CreateToolTipForRect(x_scale_box,hInst,L"scale for x-axis");
+
+    caption_box = CreateWindow(L"STATIC",
+      L"y-scale",
+      WS_CHILD | WS_VISIBLE | SS_NOTIFY,
+      465,202,47,17,
       hWnd, NULL,NULL, NULL);
+    SendMessage(caption_box,WM_SETFONT,(WPARAM)standard_font,TRUE);
+    CreateToolTipForRect(caption_box,hInst,L"scale for y-axis");
+    CreateToolTipForRect(y_scale_box,hInst,L"scale for y-axis");
+
+
     text_box = CreateWindow(L"STATIC",
       L"If you can see this something is not working.",
       WS_CHILD | WS_VISIBLE,
       10,220,400,100,
       hWnd, NULL,NULL, NULL);
+
+    SetWindowLongPtr(text_box,-4,(LONG_PTR)&TextBoxProc);
     break;
 
   case WM_COMMAND:
@@ -397,7 +502,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
       double duration =(std::chrono::duration<double, std::micro>(t_end-t_start).count());
 
       if ((double)duration+1000>1000*1000/(fps)){
-        double cur_fps=1*1000*1000/duration;
+        cur_fps=1*1000*1000/duration;
         double mcpi2=m_cpi/2.54;
         double usex=lastx;
         double usey=lasty;
@@ -416,12 +521,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
           speed_unit_str=speed_unit_str_cpms;
           move_unit_str=move_unit_str_cpms;
         }
-        hResult = StringCchPrintf(global_temp_string, STRSAFE_MAX_CCH, TEXT("mouse_speed=%.3f %s\r\nend_sensitivity=%.3f %s\r\nfps=%.0f\r\nRaw mouse data: \r\nlastX=%d lastY=%d \r\n"), 
+        hResult = StringCchPrintf(info_string, STRSAFE_MAX_CCH, TEXT("mouse speed: %.3f %s\r\nmultiplier: %.3f %s\r\nfps: %.0f\r\n"), 
           speed, speed_unit_str,
           multi, move_unit_str,
-          cur_fps,
-          lastx,
-          lasty);
+          cur_fps);
 
         InvalidateRect(hWnd,&invalidate_rect,false);
 
@@ -429,8 +532,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
           // TODO: write error handler
         }
-        // OutputDebugString(global_temp_string);
-        Static_SetText(text_box, global_temp_string);
+        // OutputDebugString(info_string);
+        // Static_SetText(text_box, info_string);
+        InvalidateRect(text_box, &invalidate_rect_2,false);
+
         t_start =t_end;
         lastx=0;
         lasty=0;
@@ -456,12 +561,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     hResult = StringCchPrintf(global_temp_string, STRSAFE_MAX_CCH, TEXT("%.0f (%s)"), y_max, move_unit_str);
     SetTextColor(hdc_backbuffer,0x00BBBBBB);
     SetBkColor(hdc_backbuffer,0x00000000);
-    TextOut(hdc_backbuffer, 0, 0, global_temp_string, wcslen(global_temp_string));
-    TextOut(hdc_backbuffer, 0, invalidate_rect.bottom-27, L"0", 1);
+    SelectObject(hdc_backbuffer,standard_font);
+    TextOut(hdc_backbuffer, 3, 0, global_temp_string, wcslen(global_temp_string));
+    TextOut(hdc_backbuffer, 3, invalidate_rect.bottom-27, L"0", 1);
 
     hResult = StringCchPrintf(global_temp_string, STRSAFE_MAX_CCH, TEXT("(%s) %.0f"),  speed_unit_str, x_max);
     SetTextAlign(hdc_backbuffer,TA_RIGHT);
-    TextOut(hdc_backbuffer, invalidate_rect.right-10, invalidate_rect.bottom-27, global_temp_string, wcslen(global_temp_string));
+    TextOut(hdc_backbuffer, invalidate_rect.right-13, invalidate_rect.bottom-27, global_temp_string, wcslen(global_temp_string));
 
 
 
