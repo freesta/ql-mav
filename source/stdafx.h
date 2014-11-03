e0221ce73b2f72beef2a8db405af111db698e3a2
freesta <freesta1@gmx.de>
2014-11-03 12:53:45 +0100

    Background color fixes

--- source/stdafx.h
+++ source/stdafx.h
@@ -7,7 +7,7 @@
 #pragma comment(linker,"\"/manifestdependency:type='win32' \
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
 processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-// #pragma comment(lib, "comctl32.lib") // This was needed for subclassing, might readd later.
+#pragma comment(lib, "comctl32.lib") // This was needed for subclassing, might readd later.
 
 #include "targetver.h"
 
